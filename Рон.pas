b85291838d8314477585e4ff3e82d80b74a2114f
prospero78 <prospero.78.su@gmail.com>
2016-10-11 14:33:26 +0200

    04 Модуль регистра общего назначения.

---
+++ Рон.pas
@@ -0,0 +1,21 @@
+﻿unit Рон;
+(*
+
+Регистр общео назначения.
+Описан как тип.
+
+*)
+interface
+
+type
+   тРон = class
+   private 
+      _знач: integer;
+      procedure _знач_Уст(знач_: integer);
+   public 
+      property знач: integer read _знач write _знач_Уст;
+   end;
+
+implementation
+
+end.
